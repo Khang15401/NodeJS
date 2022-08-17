6d2bbba27477424b8d79faeb115368a6f8a19851
Khang15401 <akhang147@gmail.com>
2022-08-17 22:20:49 +0700

    cai dat

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/tin-tuc', (req, res) => {
+  var a = 1;
+  var b = 2;
+  var c = a + b;
+  var d = c + a;
+
+
+  return res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening http://localhost:${port}`)
+})
