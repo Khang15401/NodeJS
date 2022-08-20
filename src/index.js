a7caefa06d09220fe5e602259f866e0c57930c87
Khang15401 <akhang147@gmail.com>
2022-08-20 23:24:26 +0700

    param

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', hbs.engine({
@@ -24,6 +24,11 @@ app.get('/', (req, res) => {
 
 app.get('/news', (req, res) => {
   res.render('news');
+})
+
+app.get('/search', (req, res) => {
+  console.log(req.query);
+  res.render('search');
 })
 
 app.listen(port, () => {
