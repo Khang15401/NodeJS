e5f89dff0fa5a33c3dcdfd414028d5686b629b4c
Khang15401 <akhang147@gmail.com>
2022-08-30 11:04:16 +0700

    husky

--- src/index.js
+++ src/index.js
@@ -31,6 +31,6 @@ app.set('views', path.join(__dirname, 'resources', 'views'));
 //Route init
 route(app);
 
-        app.listen(port, () => {
-        console.log(`Example app listening http://localhost:${port}`);
-        });
+    app.listen(port, () => {
+    console.log(`Example app listening http://localhost:${port}`);
+    });
