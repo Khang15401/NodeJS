559c834ba291b6d4549240c582cdefe727ed8809
Khang15401 <akhang147@gmail.com>
2022-08-30 10:47:41 +0700

    husky-prettier-lint-staged

--- src/index.js
+++ src/index.js
@@ -4,28 +4,33 @@ const morgan = require('morgan');
 const hbs = require('express-handlebars');
 const app = express();
 const port = 3000;
-const route = require('./routes')
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 // HTTP logger
 // app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', hbs.engine({
-    extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    hbs.engine({
+        extname: '.hbs',
+    }),
+);
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources', 'views'));
+        app.set('views', path.join(__dirname, 'resources', 'views'));
 
 //Route init
-route(app);
+route(      app);
 
-app.listen(port, () => {
-  console.log(`Example app listening http://localhost:${port}`)
-})
+      app.listen(port, () => {
+          console.log(`Example app listening http://localhost:${port}`);
+      });
