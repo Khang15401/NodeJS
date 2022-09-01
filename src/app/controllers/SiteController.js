6a90138b46bae4b01fe2fffb831db2f34425f696
Khang15401 <akhang147@gmail.com>
2022-09-01 20:25:56 +0700

    C-RUD DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,18 +1,19 @@
 const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../until/mongoose');
 
 class SiteController {
     
     // [GET] /
-    index(req, res) {
+    index(req, res, next) {
         
-        Course.find({}, function (err, courses) {
-            if (!err) {
-                res.json(courses);
-            } else {
-                res.status(400).json({error: 'error'});
-            }
+        Course.find({}) 
+            .then(courses => {
+                res.render('home', { 
+                    courses : mutipleMongooseToObject(courses)
+                });
+            })
             
-        });
+            .catch(next);
         // res.render('home');
     }
 
