7f357f58b66fccf2287478196bdc72435004d683
NguyenAnKhang <khangb1910234@student.ctu.edu.vn>
2022-10-01 16:24:53 +0700

    select-all

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -3,18 +3,23 @@ const { mutipleMongooseToObject } = require('../../until/mongoose');
 
 class MeController {
     // [GET] /me/stored/courses
+    
     storedCourses(req,res, next) {
-        Course.find({})
-            .then(courses => res.render('me/stored-Courses', {
-                courses: mutipleMongooseToObject(courses)
-            }))
+        Promise.all ([Course.find({}), Course.countDocumentsDeleted()]) 
+            .then(([courses, deleteCount]) => 
+                res.render('me/stored-courses', {
+                    deleteCount,
+                    courses: mutipleMongooseToObject(courses),
+                })
+            )
             .catch(next);
+
     }
 
     // [GET] /me/trash/courses
     trashCourses(req, res, next) {
         Course.findDeleted({})
-            .then(courses => res.render('me/trash-Courses', {
+            .then(courses => res.render('me/trash-courses', {
                 courses: mutipleMongooseToObject(courses)
             }))
             .catch(next);
